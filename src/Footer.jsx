426196c0361e4229afb18146991875521a456805
Echo <echopossum@gmail.com>
2024-12-28 18:26:26 -0500

    Complete Chapter 8

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({length}) => {
     const today = new Date()
   
     return (
     <footer>
-        <p>Copyright {today.getFullYear()}</p>
+        <p>{length} List { length === 1 ? 'Item' : 'Items'}</p>
     </footer>
   )
 }
