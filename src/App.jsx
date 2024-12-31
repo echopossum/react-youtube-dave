2b2ecfffdf4a02b15248690284490bb0694f5b1d
Echo <echopossum@gmail.com>
2024-12-31 00:57:01 -0500

    Ran in neovim let's see what broke lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,69 @@
-import Header from './Header'
-import AddItem from './AddItem'
-import SearchItem from './SearchItem'
-import Content from './Content'
-import Footer from './Footer'
-import { useState } from 'react'
+import Header from "./Header";
+import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')))
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist")),
+  );
 
-  const [newItem, setNewItem] = useState('')
+  const [newItem, setNewItem] = useState("");
 
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
 
   const setAndSaveItems = (newItems) => {
-    setItems(newItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(newItems))
-  }
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  };
 
   const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1
-    const myNewItem = {id, checked:false, item}
-    const listItems = [...items,myNewItem]
-    setAndSaveItems(listItems)
-
-  }
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
 
   const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? {...item,checked:!item.checked} : item)
-    setAndSaveItems(listItems)
-  }
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item,
+    );
+    setAndSaveItems(listItems);
+  };
 
   const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id)
-    setAndSaveItems(listItems)
-  }
+    const listItems = items.filter((item) => item.id !== id);
+    setAndSaveItems(listItems);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if(!newItem) return
-    addItem(newItem)
-    setNewItem('')
-  }
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
 
   return (
-    <div className='App'>
-      <Header title='Grocery List'/>
-      <AddItem 
+    <div className="App">
+      <Header title="Grocery List" />
+      <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-       <SearchItem
-        search = {search}
-        setSearch = {setSearch}
+      <SearchItem search={search} setSearch={setSearch} />
+      <Content
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLocaleLowerCase()),
+        )}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
       />
-      <Content 
-        items = {items.filter(item => ((item.item).toLowerCase()).includes
-          (search.toLocaleLowerCase()))}
-        handleCheck = {handleCheck}
-        handleDelete = {handleDelete}
-      />
-      <Footer 
-        length = {items.length}
-      />
+      <Footer length={items.length} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
